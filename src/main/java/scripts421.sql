7f499218ccb31298fe32ba2b3ea40ce988a0e328
Drachlo <deuse.sec@gmail.com>
2025-03-12 22:10:19 +0300

    sql_scripts 1.2

--- src/main/java/scripts421.sql
+++ src/main/java/scripts421.sql
@@ -1,16 +1,10 @@
-ALTER TABLE student ADD CONSTRAINT age_CONSTRAINT CHECK (age > 16)
+ALTER TABLE student ADD CONSTRAINT age_CONSTRAINT CHECK (age > 16);
 
-ALTER TABLE student ADD CONSTRAINT name_UNIQUE UNIQUE (name)
+ALTER TABLE student ADD CONSTRAINT name_UNIQUE UNIQUE (name);
 
-ALTER TABLE student ADD CONSTRAINT name_not_null CHECK (name is not null)
+ALTER TABLE student ADD CONSTRAINT name_not_null CHECK (name is not null);
 
-ALTER TABLE faculty ADD CONSTRAINT name_f_unique UNIQUE (name, color)
+ALTER TABLE faculty ADD CONSTRAINT name_f_unique UNIQUE (name, color);
 
-ALTER TABLE student ALTER COLUMN age SET DEFAULT 20
+ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
 
-select s.name, s.age, f.name
-from student s, faculty f
-
-select student.name, student.avatar_id, avatar.media_type
-from student
-INNER join avatar on student.avatar_id = avatar.id
